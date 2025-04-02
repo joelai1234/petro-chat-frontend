51967c97c85a2ca151bb84f5c7708f32a8cea2cc
joe.lai <joelai1234567890@gmail.com>
2025-04-02 08:36:58 +0800

    feat: modify default conversaction

--- app/data/conversactions.tsx
+++ app/data/conversactions.tsx
@@ -6,37 +6,37 @@ const defaultConversationItems: DefaultConversationItem[] = [
     id: 'daily-report',
     name: 'Daily Report',
     icon: <ClockIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-    value: 'daily-report',
+    value: 'Give me today\'s report.',
   },
   {
     id: 'end-of-month-report',
     name: 'End of Month Report',
     icon: <CalendarIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-    value: 'end-of-month-report',
+    value: 'Show me the end-of-month report.',
   },
   {
     id: 'production-analysis',
     name: 'Production Analysis',
     icon: <ChartPieIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-    value: 'production-analysis',
+    value: 'What’s the production analysis looking like?',
   },
   {
     id: 'performance-metrics',
     name: 'Performance Metrics',
     icon: <PresentationChartBarIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-    value: 'performance-metrics',
+    value: 'How are the performance metrics?',
   },
   {
     id: 'field-operations-status',
     name: 'Field Operations Status',
     icon: <GlobeAltIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-    value: 'field-operations-status',
+    value: 'What’s the status in the field?',
   },
   // {
   //   id: 'resource-allocation',
   //   name: 'Resource Allocation',
   //   icon: <ChartBarIcon className="w-5 h-5 text-gray-400 group-hover:text-gray-500" />,
-  //   value: 'resource-allocation',
+  //   value: 'How are we doing on resource allocation?',
   // },
 ]
 
